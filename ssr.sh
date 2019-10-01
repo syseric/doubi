89d70f0aeea087e7174e94e535d35bf5bcb49d68
syseric <987644863@qq.com>
2019-10-01 22:57:22 +0800

    Update ssr.sh
    修正libsodium

--- ssr.sh
+++ ssr.sh
@@ -759,26 +759,26 @@ Install_Libsodium(){
 		echo -e "${Info} 安装依赖..."
 		yum -y groupinstall "Development Tools"
 		echo -e "${Info} 下载..."
-		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/releases/download/${Libsodiumr_ver}/libsodium-${Libsodiumr_ver}.tar.gz"
+		wget  --no-check-certificate -N "https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 		echo -e "${Info} 解压..."
-		tar -xzf libsodium-${Libsodiumr_ver}.tar.gz && cd libsodium-${Libsodiumr_ver}
+		tar xzvf LATEST.tar.gz && cd libsodium*
 		echo -e "${Info} 编译安装..."
-		./configure --disable-maintainer-mode && make -j2 && make install
+		./configure --disable-maintainer-mode && make -j8 && make install
 		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	else
 		apt-get update
 		echo -e "${Info} 安装依赖..."
 		apt-get install -y build-essential
 		echo -e "${Info} 下载..."
-		wget  --no-check-certificate -N "https://github.com/jedisct1/libsodium/releases/download/${Libsodiumr_ver}/libsodium-${Libsodiumr_ver}.tar.gz"
+		wget  --no-check-certificate -N "https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 		echo -e "${Info} 解压..."
-		tar -xzf libsodium-${Libsodiumr_ver}.tar.gz && cd libsodium-${Libsodiumr_ver}
+		tar xzvf LATEST.tar.gz && cd libsodium*
 		echo -e "${Info} 编译安装..."
-		./configure --disable-maintainer-mode && make -j2 && make install
+		./configure --disable-maintainer-mode && make -j8 && make install
+		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	fi
 	ldconfig
-	cd .. && rm -rf libsodium-${Libsodiumr_ver}.tar.gz && rm -rf libsodium-${Libsodiumr_ver}
-	[[ ! -e ${Libsodiumr_file} ]] && echo -e "${Error} libsodium 安装失败 !" && exit 1
+	cd .. && rm -rf LATEST.tar.gz && rm -rf libsodium*
 	echo && echo -e "${Info} libsodium 安装成功 !" && echo
 }
 # 显示 连接信息
